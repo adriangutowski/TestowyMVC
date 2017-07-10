c37bbd1174c9475fab185c27e2fb2000ad441a99
Adrian Gutowski <gutowskiap@gmail.com>
2017-07-10 08:23:19 +0200

    Dodano model Przedmiot.

---
+++ TestowaMVC/Models/Przedmiot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestowaMVC.Models
+{
+    public class Przedmiot
+    {
+        public int id { get; set; }
+        public bool whitelisted { get; set; }
+        public Buys buys { get; set; }
+        public Sells sells { get; set; }
+    }
+
+    public class Buys
+    {
+        public int quantity { get; set; }
+        public int unit_price { get; set; }
+    }
+
+    public class Sells
+    {
+        public int quantity { get; set; }
+        public int unit_price { get; set; }
+    }
+}
